e9dd36ef195c19cf13bb142972e6ca0d223db445
Victoria Banjo <67071771+banjovic@users.noreply.github.com>
2022-12-31 14:07:28 +0100

    fixed error in yarn build (user)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { auth } from "../config/firebase";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { User, onAuthStateChanged, signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 
 import styles from "../styles/pageStyles/dashboard.module.scss";
@@ -8,25 +8,54 @@ import styles from "../styles/pageStyles/dashboard.module.scss";
 type Props = {};
 
 type loggedInUserType = {
-  accessToken: string;
   displayName: string;
   email: string;
   emailVerified: boolean;
+  isAnonymous: boolean;
   phoneNumber: string;
+  photoURL: string;
+  tenantId: string;
+  uid: string;
 };
 
 const Dashboard = (props: Props) => {
   const router = useRouter();
   const [loggedInUser, setLoggedInUser] = useState<loggedInUserType>({
-    accessToken: "",
     displayName: "",
     email: "",
     emailVerified: false,
     phoneNumber: "",
+    tenantId: "",
+    uid: "",
+    photoURL: "",
+    isAnonymous: false,
   });
-  console.log("loggedInUser", loggedInUser);
 
-  onAuthStateChanged(auth, (user) => setLoggedInUser(user));
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setLoggedInUser({
+        uid: user.uid as string,
+        email: user.email as string,
+        displayName: user.displayName as string,
+        emailVerified: user.emailVerified as boolean,
+        phoneNumber: user.phoneNumber as string,
+        tenantId: user.tenantId as string,
+        photoURL: user.photoURL as string,
+        isAnonymous: user.isAnonymous as boolean,
+      });
+    } else {
+      setLoggedInUser({
+        displayName: "",
+        email: "",
+        emailVerified: false,
+        phoneNumber: "",
+        tenantId: "",
+        uid: "",
+        photoURL: "",
+        isAnonymous: false,
+      });
+    }
+  });
 
   const onClickLogout = async () => {
     try {
